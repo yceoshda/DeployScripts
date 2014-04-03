57bd4d9412f706cf3f95b4dd2e3d8fa2bc54c81b
Yceos HdA <yceoshda@gmail.com>
2014-04-03 16:52:48 +0200

    fixed server_name var for index

--- conf/index.php
+++ conf/index.php
@@ -1,10 +1,10 @@
 <!doctype html>
 <html>
 <head>
-    <title><?php echo $_SERVER['HTTP_HOST']; ?> Vhost - <?php echo gethostname(); ?></title>
+    <title><?php echo $_SERVER['SERVER_NAME']; ?> Vhost - <?php echo gethostname(); ?></title>
 </head>
 <body>
-<h1><?php echo $_SERVER['HTTP_HOST']; ?></h1>
+<h1><?php echo $_SERVER['SERVER_NAME']; ?></h1>
 <h2>Hello world</h2>
 <p>Server: <?php echo gethostname(); ?></p>
 </body>
